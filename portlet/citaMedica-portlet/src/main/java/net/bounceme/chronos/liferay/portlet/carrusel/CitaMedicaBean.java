14b0bf6140556068785b5595fbad05bc4b5e34a9
federicomartinlara <federicomartinlara1976@gmail.com>
2017-08-02 21:35:03 +0200

    Parámetros del portlet

---
+++ portlet/citaMedica-portlet/src/main/java/net/bounceme/chronos/liferay/portlet/carrusel/CitaMedicaBean.java
@@ -0,0 +1,10 @@
+package net.bounceme.chronos.liferay.portlet.carrusel;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = CitaMedicaBean.NAME)
+@RequestScoped
+public class CitaMedicaBean {
+	public static final String NAME = "citaMedicaBean";
+}
